5b904e2f377818b9654aa3af3c3ac02640b32cec
Bufo <124179824+bufo1129@users.noreply.github.com>
2023-03-27 05:45:59 +0000

    [training]todo

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,34 @@
+class TodosController < ApplicationController
+
+  def index
+    @todos = Todo.all
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.create(todo_params)
+    redirect_to root_path
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update(todo_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to root_path
+  end
+
+  private
+    def todo_params
+      params.require(:todo).permit(:title)
+    end
+
+end
